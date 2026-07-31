574a5d59b10d3ce2b228e2172b0af8d24fc6a1e3
povtoffen <schmidt.leon@outlook.de>
2026-07-31 20:21:21 +0200

    Add database dependencies to db.js

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
-node_modules/
-data/
-.env
-*.log
-.DS_Store
+const Database = require('better-sqlite3');
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
